b70fdf94ea20d454b745281d8a5cf863fd811544
ppranay20 <pranay20.parakh@gmail.com>
2025-11-23 12:54:12 +0530

    feat: database check in health route

--- app/routes/health.py
+++ app/routes/health.py
@@ -1,7 +1,23 @@
 from flask import Blueprint, jsonify
+from sqlalchemy import text
+from app.db import SessionContext
 
 bp = Blueprint("health", __name__)
 
 @bp.route("/health", methods=["GET"])
 def health():
-    return jsonify({"status": "ok"})
+    try:
+        with SessionContext() as session:
+            session.execute(text("SELECT 1"))
+        
+        return jsonify({
+            "status": "healthy", 
+        }), 200
+
+    except Exception as e:
+        print(f"Health check failed: {e}")
+        return jsonify({
+            "status": "unhealthy", 
+            "database": "disconnected", 
+            "error": "Database not connected please check you connection"
+        }), 503
